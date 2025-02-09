f58bbddaca0abdbc4fdaae440296cd52e64fba92
ryo-asashi <daysbetween627@gmail.com>
2025-02-09 20:13:45 +0900

    version 0.4.8.900 - update plot.mid.importance

--- R/plot_mid_importance.R
+++ R/plot_mid_importance.R
@@ -34,7 +34,7 @@ plot.mid.importance <- function(
     imp <- imp[1L:min(max.bars, nrow(imp), na.rm = TRUE), ]
     cols <- if (use.theme) {
       if (theme$type == "qualitative")
-        to.colors(imp$importance > 0, theme)
+        to.colors(imp$degree, theme)
       else
         to.colors(imp$importance, theme)
     } else "gray35"
